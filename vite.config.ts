353533dc81c05868ac6671060934bf523ec8da9c
hoannvbo <hoannvph37082@fpt.edu.vn>
2024-07-24 11:56:54 +0700

    gg

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        shop: resolve(__dirname, 'shop.html'),
-        detail: resolve(__dirname, 'detail-product.html'),
+        shop: resolve(__dirname, 'products.html'),
+        detail: resolve(__dirname, 'detail.html'),
         cart: resolve(__dirname, 'cart.html'),
-        pay: resolve(__dirname, 'pay-product.html'),
-        login: resolve(__dirname, 'login.html'),
+        pay: resolve(__dirname, 'checkout.html'),
       },
     },
   },
